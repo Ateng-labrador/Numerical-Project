8148dbd9e1e72b054ddd7df30b2c4b0b5278f021
Arlan Tengga <arlantengga8@gmail.com>
2026-01-10 15:58:25 +0700

    feat: menambahkan algoritma baru

--- numlib/aljabarlinear.py
+++ numlib/aljabarlinear.py
@@ -2,6 +2,7 @@ import numpy as np
 import math
 from typing import Union
 from util import error as error
+import matfuctions as mathf
 
 
 def identity(n : int = 2) -> list[list[Union[int, float]]]:
@@ -26,7 +27,7 @@ def identity(n : int = 2) -> list[list[Union[int, float]]]:
     return matriks
 
 
-def zeros(n : int , m : int = None) -> list[list[Union[int, float]]]:
+def zeros(n : int , m : int = None) -> list[list[Union[int]]]:
     """
     Fungsi untuk membuat matriks nol
 
@@ -40,10 +41,7 @@ def zeros(n : int , m : int = None) -> list[list[Union[int, float]]]:
     matriks = []
     if m == None:
         for _ in range(n):
-            row = []
-            for _ in range(n):
-                row.append(0)
-            matriks.append(row)
+            matriks.append(0)
         return matriks
     else:
         for _ in range(n):
@@ -95,52 +93,6 @@ def cross(A : Union[int, float],
         ]
     else:
         return error.IndeksError("panjang tidak sesuai")
-    
-
-def gauss_back_elim(
-        A : list[list[Union[int, float]]],
-        b : list[list[Union[int, float]]]
-        ) -> list[Union[int, float]]:
-    """
-    Fungsi untuk melakukan eliminasi gauss
-
-    parameter:
-    A : Matriks non singular
-    b : Matriks ruas kanan
-
-    return:
-    x : hasil kalkulasi fungsi
-    """
-    A = A.astype(float)
-    b = b.astype(float)
-    n = len(b)
-    for k in range(n - 1):
-        if A[k, k] == 0:
-            for i in range(k + 1, n):
-                if A[i, k] != 0:
-                    A[[k, i]] = A[[i, k]]
-                    b[[k, i]] = b[[i, k]]
-                    break
-        for i in range(k+1, n):
-            m = A[i, k] / A[k, k]
-            A[i, k+1:] -= m * A[k, k+1:]
-            b[i] -= m * b[k]
-    
-    x = zeros(n)
-    for i in range(n - 1, -1, -1):
-        x[i] = (b[i] - np.dot(A[i, i+1:], x[i+1:])) / A[i, i]
-    return x
-
-
-def subti_forward(U : list[list[Union[int, float]]],
-                  b : list[Union[int, float]]) -> list[Union[int, float]]:
-    x = [0] * len(b)
-    for i in range(len(b)-1, -1, -1):
-        sum = b[i]
-        for j in range(i+1, len(b)):
-            sum -= U[i][j] * x[j]
-        x[i] = sum / U[i][i]
-    return x
 
 
 def copy(A : Union[int, float]) -> list[list[Union[int, float]]]:
@@ -155,30 +107,6 @@ def copy(A : Union[int, float]) -> list[list[Union[int, float]]]:
         res.append(row)
     return res
 
-def LU(A : list[list[Union[int, float]]]) -> list[list[Union[int, float]]]:
-    """
-    Fungsi Untuk membuat segitiga atas dan segitiga bawah
-
-    parameter:
-    A : Matriks
-
-    return:
-    L : segitiga atas
-    U : segitiga bawah
-    
-    """
-    n = len(A)
-    L = identity(n)
-    U = copy(A)
-
-    for k in range(n-1):
-        for i in range(k+1, n):
-            if U[k, k] == 0:
-                raise ValueError("Pivot nol ditemukan.Faktorisasi LU tidak dapat dilakukan.")
-            L[i, k] = U[i, k] / U[k, k]
-            for j in range(k, n):
-                U[i, j] -= L[i, k] * U[k, j]
-    return L, U
 
 def penjumlahan(
         A : list[list[Union[int, float]]],
@@ -261,6 +189,7 @@ def perkalian(
                 result[i][j] += A[i][k] * B[k][j]
     return result
 
+
 def perkalian_skalar_matriks(
         matriks : list[list[Union[int, float]]],
         x : Union[int, float]
@@ -284,6 +213,112 @@ def perkalian_skalar_matriks(
             row.append(multi)
         result.append(row)
     return result
+
+
+def gauss_back_elim(
+        A : list[list[Union[int, float]]],
+        b : list[list[Union[int, float]]]
+        ) -> list[Union[int, float]]:
+    """
+    Fungsi untuk melakukan eliminasi gauss
+
+    parameter:
+    A : Matriks non singular
+    b : Matriks ruas kanan
+
+    return:
+    x : hasil kalkulasi fungsi
+    """
+    A = A.astype(float)
+    b = b.astype(float)
+    n = len(b)
+    for k in range(n - 1):
+        if A[k, k] == 0:
+            for i in range(k + 1, n):
+                if A[i, k] != 0:
+                    A[[k, i]] = A[[i, k]]
+                    b[[k, i]] = b[[i, k]]
+                    break
+        for i in range(k+1, n):
+            m = A[i, k] / A[k, k]
+            A[i, k+1:] -= m * A[k, k+1:]
+            b[i] -= m * b[k]
+    
+    x = zeros(n)
+    for i in range(n - 1, -1, -1):
+        x[i] = (b[i] - np.dot(A[i, i+1:], x[i+1:])) / A[i, i]
+    return x
+
+
+def subti_forward(U : list[list[Union[int, float]]],
+                  b : list[Union[int, float]]) -> list[Union[int, float]]:
+    x = [0] * len(b)
+    for i in range(len(b)-1, -1, -1):
+        sum = b[i]
+        for j in range(i+1, len(b)):
+            sum -= U[i][j] * x[j]
+        x[i] = sum / U[i][i]
+    return x
+
+
+#*
+# matriks khusus
+# matriks simetris
+# matriks definit positif
+# matriks pita (banded matrix)
+# *#
+
+def LU(A : list[list[Union[int, float]]]) -> list[list[Union[int, float]]]:
+    """
+    Fungsi Untuk membuat segitiga atas dan segitiga bawah
+
+    parameter:
+    A : Matriks
+
+    return:
+    L : segitiga atas
+    U : segitiga bawah
+    
+    """
+    n = len(A)
+    L = identity(n)
+    U = copy(A)
+
+    for k in range(n-1):
+        for i in range(k+1, n):
+            if U[k, k] == 0:
+                raise ValueError("Pivot nol ditemukan.Faktorisasi LU tidak dapat dilakukan.")
+            L[i, k] = U[i, k] / U[k, k]
+            for j in range(k, n):
+                U[i, j] -= L[i, k] * U[k, j]
+    return L, U
+
+
+def LDLT_decomposition(A : list[list[Union[int, float]]]) -> list[list]:
+    """
+    Fungsi Untuk Melakukan faktorisasi LDLT composition
+    A = LDL^T
+    
+    """
+    n = len(A)
+    L = zeros(n, n)
+    d = zeros(n)
+
+    for i in range(n):
+        sum_val = 0.0
+        for k in range(i):
+            sum_val += (L[i][k] ** 2) * d[k]
+        d[i] = A[i][i] - sum_val
+
+        for j in range(i + 1, n):
+            sum_val = 0.0
+            for k in range(i):
+                sum_val += L[j][k] * L[i][k] * d[k]
+            L[j][i] = (A[j][i] - sum_val) / d[i]
+        L[i][i] = 1
+
+    Lt = [[L[j][i] for j in range(n)] for i in range(len(A[0]))]
+    return L, d, Lt
 
 
 def rotation(matriks : list[list[Union[int, float]]],
---
+++ numlib/matfuctions.py
@@ -0,0 +1,39 @@
+from typing import Union
+import numpy as np
+from util import error as error
+
+
+def sum(x : list[list[Union[int, float]]]) -> list[list[Union[int, float]]]:
+    if isinstance(x, list) and isinstance(x[0], list):
+        r = 0
+        for i in range(len(x)):
+            for j in range(len(x[i])):
+                r += x[i][j]
+        return r
+    if isinstance(x, list):
+        r = 0
+        for i in range(len(x)):
+            r += x[i]
+        return r
+
+
+def square(x : list[list[Union[int, float]]]) -> list[list[Union[int, float]]]:
+    if isinstance(x , list) and isinstance(x[0], list):
+        result = []
+        for i in range(len(x)):
+            row = []
+            for j in range(len(x[i])):
+                A = x[i][j] * x[i][j]
+                row.append(A)
+            result.append(row)
+        return result
+    if isinstance(x, list):
+        result = []
+        for i in range(len(x)):
+            A = x[i] * x[i]
+            result.append(A)
+        return result
+                
+
+if __name__ == "__main__":
+    pass
